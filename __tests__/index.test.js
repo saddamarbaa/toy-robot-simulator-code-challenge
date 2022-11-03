61c8a8d7f404fb666688180bc0653ee5b6ae002b
saddamarbaa <saddam@rushowl.sg>
2022-11-03 12:55:21 +0700

    test: add tdd test read test case from file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,15 @@
-const { robot } = require('../src/index');
+// const { robot } = require('../src/index');
 
-test('Should yield the excepted validation pass', () => {
-  expect(robot.validate(['PLACE       3,       1,NORTH', 'MOVE', 'MOVE', 'RIGHT', 'MOVE'])).toBe(true);
-});
+const { readFromFile } = require('../src/utils/helper');
 
-test('Should fail to pass validation', () => {
-  expect(robot.validate(['PLACE       t3,       1,NORTH', 'MOVE', 'MOVE', 'RIGHT', 'MOVE'])).toBe(true);
+describe(`Read test cases from file`, function () {
+  it('Should yield success read test case from file testCase3.txt', async function () {
+    const data = await readFromFile();
+    expect(data).toContain('PLACE 1,2,EAST');
+  });
+
+  it('Should yield failed to read filet', async function () {
+    const data = await readFromFile('testCase7.txt');
+    expect(data).toBe('failed to read file');
+  });
 });
