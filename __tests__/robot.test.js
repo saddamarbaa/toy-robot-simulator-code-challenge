a68898f137d879c99409f35b61f1fda13daf2884
saddamarbaa <saddam@rushowl.sg>
2022-11-04 04:55:24 +0700

    test: add simulation logic integration test cases

--- __tests__/robot.test.js
+++ __tests__/robot.test.js
@@ -2,6 +2,18 @@ const { robot } = require('../src/robot');
 
 const { readFromFile } = require('../src/utils/helper');
 
+describe(`Validate Reading test cases from file`, () => {
+  it('Should yield success read test case from file testCase3.txt', async () => {
+    const data = await readFromFile();
+    expect(data).toContain('PLACE 1,2,EAST');
+  });
+
+  it('Should yield failed to read file', async () => {
+    const data = await readFromFile('testCase7.txt');
+    expect(data).toBe('failed to read file');
+  });
+});
+
 describe(`Validate Command Inputs`, () => {
   test('Should yield the excepted validation pass for testCase1.txt', async () => {
     expect(await robot.validate(['PLACE 0, 0,NORTH', 'MOVE', 'REPORT'], true)).toBe(true);
@@ -20,14 +32,20 @@ describe(`Validate Command Inputs`, () => {
   });
 });
 
-describe(`Validate Reading test cases from file`, () => {
-  it('Should yield success read test case from file testCase3.txt', async () => {
-    const data = await readFromFile();
-    expect(data).toContain('PLACE 1,2,EAST');
+describe(`Application simulation (Integration testing in simulation logic)`, () => {
+  test('Should read the test1 case input from file name testCase1.txt validate the logic and yield successful test pass which is "0,1,NORTH"', async () => {
+    expect(robot.init(await readFromFile('testCase1.txt'))).toBe('0,1,NORTH');
   });
 
-  it('Should yield failed to read file', async () => {
-    const data = await readFromFile('testCase7.txt');
-    expect(data).toBe('failed to read file');
+  test('Should read the test2 case input from file name testCase2.txt validate the logic and yield successful test pass which is "0,0,WEST"', async () => {
+    expect(robot.init(await readFromFile('testCase2.txt'))).toBe('0,0,WEST');
+  });
+
+  test('Should read the test3 case input from file name testCase3.txt validate the logic and yield successful test pass which is "3,3,NORTH"', async () => {
+    expect(robot.init(await readFromFile('testCase3.txt'))).toBe('3,3,NORTH');
+  });
+
+  test('Should fail to read the test case input from file name testCase4.txt ', async () => {
+    expect(robot.init(await readFromFile('testCase3m.txt'))).toBe('Invalid command');
   });
 });
